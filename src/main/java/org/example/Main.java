d9368a2f7071ccd4b423a28ee9e74ba88f6fc6b3
HJ <1744709356@qq.com>
2023-05-23 19:56:42 +0800

    项目创建

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author HJ
+ * @date 2023/5/23 19:51
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

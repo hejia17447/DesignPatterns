400291cd7b61b0202a1b7cf6dd736b356195f038
贺佳 <hejia1744709356@163.com>
2023-05-26 21:23:18 +0800

    枚举单例模式测试

--- src/test/java/org/example/singleton_pattern/EnumSingletonTest.java
+++ src/test/java/org/example/singleton_pattern/EnumSingletonTest.java
@@ -2,15 +2,12 @@ package org.example.singleton_pattern;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class EnumSingletonTest {
-    
+
     @Test
-    void test() {
+    void values() {
         EnumSingleton instance = EnumSingleton.INSTANCE;
         EnumSingleton instance1 = EnumSingleton.INSTANCE;
         System.out.println(instance == instance1);
     }
-
 }
